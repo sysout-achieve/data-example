5f1c56c90a4783111891a99d7db3f372e01c5086
MSI <0408@gmail.com>
2018-02-08 21:20:38 +0900

    update

--- dbsproject/app/src/main/java/com/example/msi/dbsproject/MainActivity.java
+++ dbsproject/app/src/main/java/com/example/msi/dbsproject/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -17,40 +19,79 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        TextView text = (TextView) findViewById(R.id.text_id);
-        TextView size_id = (TextView) findViewById(R.id.size_id);
-        EditText input = (EditText) findViewById(R.id.input);
+        final TextView text = (TextView) findViewById(R.id.text_id);
+        final TextView size_id = (TextView) findViewById(R.id.size_id);
+
+        final EditText input = (EditText) findViewById(R.id.input);
+        EditText input2 = (EditText) findViewById(R.id.input2);
+        EditText input3 = (EditText) findViewById(R.id.input3);
+
         Button btn = (Button) findViewById(R.id.btn);
+        Button btn2 = (Button) findViewById(R.id.btn2);
 
         SharedPreferences name = getSharedPreferences("name" , MODE_PRIVATE);
         SharedPreferences.Editor editor = name.edit();
         SharedPreferences size = getSharedPreferences("size", MODE_PRIVATE);
         SharedPreferences.Editor editsize = size.edit();
+        String test;
+        ArrayList<String> Al = new ArrayList<>();
 
 
-
-        String txt = input.getText().toString();
-        editor.putString("long", "padding");
-        editsize.putString("long", "라지");
-
-        editor.commit();
-        editsize.commit();
 
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText input = (EditText) findViewById(R.id.input);
-                String txt = input.getText().toString();
-                TextView text = (TextView) findViewById(R.id.text_id);
-                TextView size_id = (TextView) findViewById(R.id.size_id);
-                SharedPreferences name = getSharedPreferences("name" , MODE_PRIVATE);
-                SharedPreferences size = getSharedPreferences("size", MODE_PRIVATE);
-                text.setText(name.getString(txt,"윽"));
-                size_id.setText(size.getString(txt,"아니냐?"));
+                EditText input2 = (EditText) findViewById(R.id.input2);
+                EditText input3 = (EditText) findViewById(R.id.input3);
+
+                SharedPreferences gd = getSharedPreferences(input.getText().toString(), MODE_PRIVATE); //input이름으로 SharedPreferences 생성
+                SharedPreferences.Editor edit_gd = gd.edit();
+
+
+                edit_gd.putString("1", input2.getText().toString() );
+                edit_gd.putString("2", input3.getText().toString());
+                edit_gd.commit();
             }
         });
 
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences gd = getSharedPreferences(input.getText().toString(), MODE_PRIVATE);
+                text.setText(gd.getString("1",""));
+                size_id.setText(gd.getString("2",""));
+            }
+        });
+
+
+
+
+
+
+
+//        String txt = input.getText().toString();
+//        editor.putString("long", "padding");
+//        editsize.putString("long", "라지");
+//
+//        editor.commit();
+//        editsize.commit();
+//
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                EditText input = (EditText) findViewById(R.id.input);
+//                String txt = input.getText().toString();
+//                TextView text = (TextView) findViewById(R.id.text_id);
+//                TextView size_id = (TextView) findViewById(R.id.size_id);
+//                SharedPreferences name = getSharedPreferences("name" , MODE_PRIVATE);
+//                SharedPreferences size = getSharedPreferences("size", MODE_PRIVATE);
+//                text.setText(name.getString(txt,"윽"));
+//                size_id.setText(size.getString(txt,"아니냐?"));
+//            }
+//        });
+//
 
     }
 
